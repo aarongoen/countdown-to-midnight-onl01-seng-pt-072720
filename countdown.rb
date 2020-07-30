639d0cdbfb51dd3fcce6d3478514772eab1b3efa
Aaron Goen <aarongoen2.0@gmail.com>
2020-07-30 22:41:21 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
-def countdown
+ 
+def countdown(x)
+x = 10
+  while x > 0
+    puts "#{number} SECOND(S)!"
+    x -= 1
+  end
+  case
+    when 0
+      "HAPPY NEW YEAR!"
+    end
+end
+
+def countdown_with_sleep
+  x = 10
+    while x > 0
+    sleep(2)
+    puts "#{number} SECOND(S)!"
+    x -= 1
+    end
 end
