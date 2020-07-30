2c2c79bb83b3157b791c8df3afc7fdf1fef3e92e
Aaron Goen <aarongoen2.0@gmail.com>
2020-07-30 23:04:04 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,16 +4,16 @@
 def countdown(number)
   while number > 0
     puts "#{number} SECOND(S)!"
-    x -= 1
+    number -= 1
   end
-  return "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep
-  x = 10
-    while x > 0
+    while number > 0
     sleep(2)
     puts "#{number} SECOND(S)!"
-    x -= 1
+    number -= 1
     end
+    "HAPPY NEW YEAR!"
 end
